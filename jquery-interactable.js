f550e15569cd971038f245581734d0231c01efb0
Lyubomir Slavilov <lyubo.slavilv@gmail.com>
2012-01-20 18:10:01 +0200

    code cleanup Added $(selector).interactable('destroy')
    Signed-off-by: Lyubomir Slavilov <lyubo.slavilv@gmail.com>

--- jquery-interactable.js
+++ jquery-interactable.js
@@ -1,16 +1,17 @@
 /*
+jQuery Interactable
 author: Lyubomir Slavilov
 */
 (function($){
 	//CLASSES
 	$.interactableVector = function(x, y){
-		length = function(vector){
+		var length = function(vector){
 			return Math.sqrt(vector.x*vector.x + vector.y*vector.y);
 		}
 		return {
 			x: x,
 			y: y,
-			w: Math.sqrt(x*x + y*y),
+			w: length(this),
 			add: function(vector){
 				this.x = this.x + vector.x;
 				this.y = this.y + vector.y;
@@ -41,7 +42,7 @@ author: Lyubomir Slavilov
 		KI.options = options;
 		
 		//update()
-		KI.update = function(newX, newY, speedFactor){
+		KI.update = function(newX, newY){
 			var timeDelta = parseInt(new Date().getTime()) - this.time;
 			if(timeDelta == 0) return this;
 			var oldSpace = this.space;
@@ -52,7 +53,7 @@ author: Lyubomir Slavilov
 			this.space.delta = $.interactableVector(newX - oldSpace.x, newY - oldSpace.y);
 
 			//speed
-			this.speed = this.space.delta.scale(speedFactor/timeDelta);
+			this.speed = this.space.delta.scale(1/timeDelta);
 			this.speed.delta = $.interactableVector(this.speed.x - oldSpeed.x, this.speed.y - oldSpeed.y);
 
 			//acceleration
@@ -68,79 +69,93 @@ author: Lyubomir Slavilov
 		init: function(options){
 		  
 			var
-			$this = $(this),
 			$window = $(window),
 			wdata = $(window).data('interactable');
 		  
-			//intitialize the window object
-			if($window[0]+'interactable' === $this[0]+'interactable'){
+			
+			if($window[0] === this[0]){
+				//intitialize the window object
 				options = $.extend({
-					friction: 1,
-					speedFactor:1
+					friction: 1
 				}, options);
 			  
 				if(!wdata){
-					
 					wdata= {
 						kinematics: $.interactableKinematics(options)
 					};
 					$window.data('interactable', wdata);					
 					$window.bind('mousemove.interactable', function(e){
-						var KI = $window.data('interactable').kinematics;					
-						$window.data('interactable').kinematics.update(e.pageX, e.pageY, options.speedFactor);
-						/*
-						$window.data('interactable',{
-							kinematics: KI
-						});
-						*/
-					}); //bind mousemove.tooltip 
+						$window.data('interactable').kinematics.update(e.pageX, e.pageY);
+					});
 				}
+				return this;
 			}else{
 				//initialize other objects
-				options = $.extend({
-					drag:{
-						stop:function(){}
-					},
-					axis: 'both',
-					mass: 10
-				}, options);
 				
-				var corrX = (options.axis == 'both' || options.axis == 'x') ? 1:0;
-				var corrY = (options.axis == 'both' || options.axis == 'y') ? 1:0;
-				
-				var origStop = options.drag.stop
-				var customStop = function(){
-					var ki = $window.data('interactable').kinematics;
-					var a = 0.003*Math.abs(ki.options.friction); //do not allow negative friction
+				return this.each(function(){
+					var $that = $(this);
 					
-					//TODO add mass feature to the objects :)
+					//fixes some hilarious bug - the DOMWindow object is in the set ???
+					if($that[0] == $window) return;
 					
-					if(a==0) a = 0.00001; //almost no friction
-					var t =  ki.speed.w/a;
-					var s = ki.speed.w*t - t*t*a/2;
-					console.log(
-						'a: '+a+'\n'+'v: '+ki.speed.w+'\n'+'t: '+t+'\n'+'s: '+s
-					);
-					var newPosition = ki.speed.direction().scale(s);
-					$this.animate({
-						left: $this.position().left + newPosition.x*corrX,
-						top: $this.position().top + newPosition.y*corrY
-					}, t, 'easeOutQuad');
-					if(typeof(origStop) == 'function') origStop();
-				};
-				
-				//handle the dragable
-				var origStart = options.drag.start;
-				var customStart = function(){
-					//stop all animations
-					$this.stop();
-					if(typeof(origStart) == 'function') origStart();
-				}
-				options.drag.stop = customStop;
-				options.drag.start = customStart;
-				$this.draggable(options.drag);
+					options = $.extend({
+						drag:{
+							stop:function(){}
+						},
+						axis: 'both',
+						mass: 1
+					}, options);
+					$that.addClass('interactable');
+					$that.attr('position', 'absolute');
+					//used to prevent movements by axis if the other axis is set in options
+					var maskX = (options.axis == 'both' || options.axis == 'x') ? 1:0;
+					var maskY = (options.axis == 'both' || options.axis == 'y') ? 1:0;
+
+					var customStop = options.drag.stop
+
+					//make it dragable
+
+					//extend the draggable stop method
+					var extendedStop = function(event, ui){
+						var ki = $window.data('interactable').kinematics;
+						var a = 0.003*Math.abs(ki.options.friction)*options.mass; //do not allow negative friction
+						
+						//TODO add mass feature of the objects into the game 
+
+						if(a==0) a = 0.00001; //almost no friction
+						var t =  ki.speed.w/a;
+						var s = ki.speed.w*t - t*t*a/2;
+						var newPosition = ki.speed.direction().scale(s);
+						if(!ui) return;
+						$(this).animate({
+							left: ui.position.left + newPosition.x*maskX,
+							top: ui.position.top + newPosition.y*maskY
+						}, t, 'easeOutQuad');
+						//callout the user defined stop method
+						if(typeof(customStop) == 'function') customStop();
+					};
+
+					//extend the draggable start method
+					var customStart = options.drag.start;
+					var extendedStart = function(){
+						//stop all animations
+						$(this).stop();
+						if(typeof(customStart) == 'function') customStart();
+					}
+					options.drag.stop = extendedStop;
+					options.drag.start = extendedStart;
+					$that.draggable(options.drag);
+				});
 			}
-			return this;
+		},
+		destroy: function(){
+			if($(this)[0] === $(window)[0]){
+				$('.interactable').interactable('destroy');
+				$(window).unbind('.interactable');
+				$(window).removeData('interactable')
+			}else{
+				$(this).draggable('destroy');
+			}
 		}
 	};
 	
